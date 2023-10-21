51c218bdd99eb01a7f2069941cfa756b2dc2f92d
Gevorg Minasian <cool.port-965@yandex.ru>
2023-10-21 19:03:18 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,53 @@
+# N = int(input("Введите целое число N: "))
+#
+# sum = 0.0
+# for i in range(1, N+1):
+#     sum += 1/i
+#
+# print("Сумма ряда: ", sum)
+
+# N = int(input("Введите целое число N: "))
+#
+# sum = 0
+# for i in range(N, 2*N + 1):
+#     sum += i**2
+#
+# print("Сумма ряда: ", sum)
+
+# N = int(input("Введите целое число N: "))
+#
+# product = 1.0
+# for i in range(1, N+1):
+#     product *= 1.0 + i/10.0
+#
+# print("Произведение ряда: ", product)
+
+# N = int(input("Введите целое число N: "))
+#
+# result = 0.0
+# sign = 1
+# for i in range(1, N+1):
+#     result += sign * i/10.0
+#     sign *= -1
+#
+# print("Значение выражения: ", result)
+
+
+# N = int(input("Введите целое число N: "))
+#
+# sum = 0
+# for i in range(1, 2*N, 2):
+#     sum += i
+#     print("Текущая сумма: ", sum)
+#
+# print("Квадрат числа N: ", sum)
+
+
+# A = float(input("Введите вещественное число A: "))
+# N = int(input("Введите целое число N: "))
+#
+# result = 1
+# for i in range(N):
+#     result *= A
+#
+# print("A в степени N: ", result)
